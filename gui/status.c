2b57dbc48133e20920a1840c9c030e131cc9dc4c
luke8086 <55237178+luke8086@users.noreply.github.com>
2026-03-31 23:17:55 +0200

    GUI: Simplify code of status bar

--- gui/status.c
+++ gui/status.c
@@ -22,8 +22,7 @@ enum {
     TEXT_MAX_LEN = (STATUS_WIDTH / FONT_WIDTH) - 2,
 };
 
-static window_st window;
-
+static point_st origin = { 0, GUI_HEIGHT - STATUS_HEIGHT };
 static uint16_t status_text_len = 0;
 static char status_text_buf[TEXT_MAX_LEN + 1];
 
@@ -34,7 +33,7 @@ gui_status_set_text(const char *text)
     font_st *font = &fonts[0];
     rect_st clear_rect, text_rect;
 
-    gui_surface_draw_str(&window.origin, TEXT_X, TEXT_Y, font, text, COLOR_FG, COLOR_BG);
+    gui_surface_draw_str(&origin, TEXT_X, TEXT_Y, font, text, COLOR_FG, COLOR_BG);
 
     /* If the new text is shorter than previous, clear the remaining space */
     if (len < status_text_len) {
@@ -45,7 +44,7 @@ gui_status_set_text(const char *text)
             font->size.height
         );
 
-        gui_surface_draw_rect(&window.origin, &clear_rect, COLOR_BG);
+        gui_surface_draw_rect(&origin, &clear_rect, COLOR_BG);
     }
 
     gui_rect_init(&text_rect,
@@ -55,7 +54,7 @@ gui_status_set_text(const char *text)
         font->size.height
     );
 
-    gui_wm_render_window_region(&window.origin, &text_rect);
+    gui_wm_render_window_region(&origin, &text_rect);
 
     status_text_len = len;
 }
@@ -77,12 +76,6 @@ gui_status_init(void)
 {
     memset(status_text_buf, 0, sizeof(status_text_buf));
 
-    window.size.width = STATUS_WIDTH;
-    window.size.height = STATUS_HEIGHT;
-    window.origin.x = 0;
-    window.origin.y = GUI_HEIGHT - STATUS_HEIGHT;
-    window.visible = 1;
-
-    gui_surface_draw_h_seg(&window.origin, 0, 0, STATUS_WIDTH, COLOR_FG);
+    gui_surface_draw_h_seg(&origin, 0, 0, STATUS_WIDTH, COLOR_FG);
     gui_status_set("");
 }
