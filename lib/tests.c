4b241be4b85cb56a82283029dfcdcd3c762e6b17
luke8086 <55237178+luke8086@users.noreply.github.com>
2026-04-14 20:37:26 +0200

    Lib: Improve test output

--- lib/tests.c
+++ lib/tests.c
@@ -14,6 +14,22 @@ static int tests_failed = 0;
 static int tests_ok = 0;
 
 static void
+start_test_case(const char *name)
+{
+    tests_failed = 0;
+    tests_ok = 0;
+    krn_debug_printf("- Testing %s: ", name);
+}
+
+static void
+end_test_case()
+{
+    int total = tests_ok + tests_failed;
+
+    krn_debug_printf("%d/%d ok\n", tests_ok, total);
+}
+
+static void
 check_math32(const char *name, math32_fn fn, int32_t a, int32_t b,
     int32_t expected, int expect_error)
 {
@@ -35,10 +51,11 @@ check_math32(const char *name, math32_fn fn, int32_t a, int32_t b,
     }
 }
 
-
 static void
 test_add32(void)
 {
+    start_test_case("add32");
+
     check_math32("add32", add32, 0, 0, 0, 0);
     check_math32("add32", add32, 1, 2, 3, 0);
     check_math32("add32", add32, -1, -2, -3, 0);
@@ -51,11 +68,15 @@ test_add32(void)
     check_math32("add32", add32, INT32_MIN, -1, 0, 1);
     check_math32("add32", add32, INT32_MAX, INT32_MAX, 0, 1);
     check_math32("add32", add32, INT32_MIN, INT32_MIN, 0, 1);
+
+    end_test_case();
 }
 
 static void
 test_sub32(void)
 {
+    start_test_case("sub32");
+
     check_math32("sub32", sub32, 0, 0, 0, 0);
     check_math32("sub32", sub32, 3, 2, 1, 0);
     check_math32("sub32", sub32, -1, -2, 1, 0);
@@ -67,11 +88,15 @@ test_sub32(void)
     check_math32("sub32", sub32, INT32_MIN, INT32_MIN, 0, 0);
     check_math32("sub32", sub32, INT32_MAX, -1, 0, 1);
     check_math32("sub32", sub32, INT32_MIN, 1, 0, 1);
+
+    end_test_case();
 }
 
 static void
 test_mul32(void)
 {
+    start_test_case("mul32");
+
     check_math32("mul32", mul32, 0, 0, 0, 0);
     check_math32("mul32", mul32, 1, 0, 0, 0);
     check_math32("mul32", mul32, 0, 1, 0, 0);
@@ -88,11 +113,15 @@ test_mul32(void)
     check_math32("mul32", mul32, INT32_MAX, 2, 0, 1);
     check_math32("mul32", mul32, INT32_MAX, INT32_MAX, 0, 1);
     check_math32("mul32", mul32, INT32_MIN, INT32_MIN, 0, 1);
+
+    end_test_case();
 }
 
 static void
 test_div32(void)
 {
+    start_test_case("div32");
+
     check_math32("div32", div32, 0, 1, 0, 0);
     check_math32("div32", div32, 6, 3, 2, 0);
     check_math32("div32", div32, 7, 3, 2, 0);
@@ -105,11 +134,15 @@ test_div32(void)
     check_math32("div32", div32, INT32_MIN, -1, 0, 1);
     check_math32("div32", div32, 1, 0, 0, 1);
     check_math32("div32", div32, 0, 0, 0, 1);
+
+    end_test_case();
 }
 
 static void
 test_append32(void)
 {
+    start_test_case("append32");
+
     check_math32("append32", append32, 0, 0, 0, 0);
     check_math32("append32", append32, 0, 5, 5, 0);
     check_math32("append32", append32, 1, 2, 12, 0);
@@ -123,6 +156,8 @@ test_append32(void)
     check_math32("append32", append32, INT32_MAX, 0, 0, 1);
     check_math32("append32", append32, 0, -1, 0, 1);
     check_math32("append32", append32, 0, 10, 0, 1);
+
+    end_test_case();
 }
 
 global void
@@ -138,8 +173,7 @@ tests_run(void)
     test_div32();
     test_append32();
 
-    krn_debug_printf("Tests done: %d ok, %d failed\n", tests_ok, tests_failed);
-    krn_debug_printf("Press enter to continue...");
+    krn_debug_printf("Tests done. Press enter to continue...");
     (void)bios_getc();
 }
 
