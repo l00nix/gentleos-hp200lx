04421477af9c26bf18bcf447d03a8f14f7ba2aec
luke8086 <55237178+luke8086@users.noreply.github.com>
2026-04-16 22:27:58 +0200

    Add tool to ensure CRLF line endings and fix remaining files

---
+++ tools/fixlns.pl
@@ -0,0 +1,60 @@
+#
+# Copyright (c) 2026 luke8086
+# Distributed under the terms of GPL-2 License.
+#
+# File: fixlns.pl - Fix line endings in source code
+#
+
+my @GLOBS = (
+    "apps/*",
+    "boot/*",
+    "data/*",
+    "gui/*",
+    "include/*",
+    "kernel/*",
+    "lib/*",
+    "misc/*",
+    "tools/*.pl",
+);
+
+sub slurp {
+    my ($path) = @_;
+    open(my $f, "<", $path) or die "Cannot read $path: $!\n";
+    binmode $f;
+    local $/;
+    my $data = <$f>;
+    close $f;
+    return $data;
+}
+
+sub spit {
+    my ($path, $data) = @_;
+    open(my $f, ">", $path) or die "Cannot write $path: $!\n";
+    binmode $f;
+    print $f $data;
+    close $f or die "Write error on $path\n";
+}
+
+sub collect_files {
+    my @files;
+    foreach my $g (@GLOBS) {
+        push @files, grep { -f $_ } glob($g);
+    }
+    return sort @files;
+}
+
+sub main {
+    foreach my $path (collect_files()) {
+        my $old = slurp($path);
+        my $new = $old;
+        $new =~ s/\r\n/\n/g;
+        $new =~ s/\n/\r\n/g;
+
+        next if $new eq $old;
+
+        print "Fixing $path\n";
+        spit($path, $new)
+    }
+}
+
+main();
--- tools/regen.bat
+++ tools/regen.bat
@@ -1,3 +1,4 @@
+perl tools/fixlns.pl
 perl tools/procpbm.pl
 perl tools/cproto.pl
 perl tools/genmake.pl
